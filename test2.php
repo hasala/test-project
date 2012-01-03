1b24a7aaa440c8df6bf868c9f58f53f445874134
Hasala Sandaruwan <hasalasandaruwan@gmail.com>
2012-01-03 11:38:36 +0530

    second commit

---
+++ test2.php
@@ -0,0 +1,21 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+   <?php
+        $name=$_POST['name'];
+        echo $name;
+?>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+   <input type="text" name="name"><br>
+   <input type="submit" name="submit" value="Submit Form"><br>
+</form>
+        </body>
+</html>
